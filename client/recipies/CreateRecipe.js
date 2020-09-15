610ed5733fe85cb45369c1ab98b205e622adb5b0
Vicente Losada <vi_losada@hotmail.com>
2020-09-15 17:17:04 -0500

    add multiline input

--- client/recipies/CreateRecipe.js
+++ client/recipies/CreateRecipe.js
@@ -2,19 +2,18 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-
+import InputAdornment from "@material-ui/core/InputAdornment";
 import InputBase from "@material-ui/core/InputBase";
+import { useMediaQuery } from "react-responsive";
 
 const useStyles = makeStyles((theme) => ({
   title: {
     marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(2),
   },
-  textField: {
+  textInput: {
     backgroundColor: "rgba(255, 255, 249, 1)",
-    margin: `${theme.spacing(4)}px ${theme.spacing(2)}px ${theme.spacing(3)}px`,
-    width: `${4 * 95}px`,
-    height: "44px",
+    margin: `${theme.spacing(1)}px 0`,
     padding: `0 ${theme.spacing(3)}px`,
     color: "#000",
     border: "none",
@@ -25,6 +24,18 @@ const useStyles = makeStyles((theme) => ({
       backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='22' ry='22' stroke='black' stroke-width='4.8' stroke-dasharray='5%2c 4' stroke-dashoffset='0' stroke-linecap='butt'/%3e%3c/svg%3e")`,
     },
   },
+  textField: {
+    width: `${4 * 82}px`,
+    height: "44px",
+  },
+  multiline: {
+    width: `100%`,
+    height: `${24 * 5 + 8}px`,
+  },
+  multilineMobile: {
+    width: `${4 * 82}px`,
+    height: `${24 * 5 + 8}px`,
+  },
   focused: {
     border: "none",
     backgroundColor: "rgba(255, 255, 255, 1)",
@@ -34,6 +45,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function CreateRecipe() {
   const classes = useStyles();
+  const isMobile = useMediaQuery({ query: "(max-width: 600px)" });
 
   return (
     <Container maxWidth="md">
@@ -41,9 +53,18 @@ export default function CreateRecipe() {
         Create Recipe
       </Typography>
       <InputBase
-        classes={{ root: classes.textField, focused: classes.focused }}
-        defaultValue="Naked input"
-        inputProps={{ "aria-label": "naked" }}
+        className={classes.textField}
+        classes={{ root: classes.textInput, focused: classes.focused }}
+        inputProps={{ "aria-label": "name" }}
+        startAdornment={<InputAdornment position="start">Name:</InputAdornment>}
+      />
+      <InputBase
+        className={isMobile ? classes.multilineMobile : classes.multiline}
+        classes={{ root: classes.textInput, focused: classes.focused }}
+        inputProps={{ "aria-label": "description" }}
+        multiline
+        rows={5}
+        placeholder="Description"
       />
     </Container>
   );
